828e890ad1f5fdbd8016ce701ab45a4384ff18a4
webzyro <gmbworkswebzyro@gmail.com>
2025-12-09 18:04:25 +0530

    added build file

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -26,10 +26,10 @@ $(document).ready(function () {
         animateOut: "slideOutLeft",
         responsive: {
             0: {
-                items: 2,
+                items: 1,
             },
             768: {
-                items: 3,
+                items: 2,
             },
             992: {
                 items: 3,
