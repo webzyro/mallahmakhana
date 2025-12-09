e63d19e846be55085fd621248a845500cf12f1a9
webzyro <gmbworkswebzyro@gmail.com>
2025-12-09 16:41:10 +0530

    some changes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,9 +13,6 @@
     {{-- CSS (Vite, Tailwind, Bootstrap, etc.) --}}
     @vite(['resources/js/app.js'])
 
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/modern-cart.css') }}">
-
     {{-- Bootstrap Link --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous" />
@@ -29,6 +26,9 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/modern-cart.css') }}">
 
 </head>
 
