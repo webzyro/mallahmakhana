232e70879be221d3ca1e0d0d0b67e4ddc358c9e7
webzyro <gmbworkswebzyro@gmail.com>
2025-12-17 15:51:17 +0530

    reform adminprovider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,6 +50,8 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+            ])
+             ->authMiddleware([
                 Authenticate::class,
             ]);
     }
