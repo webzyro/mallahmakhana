1bb5f0333c49960c184f2c2a66a3d199f4f082c1
webzyro <gmbworkswebzyro@gmail.com>
2025-12-18 15:46:33 +0530

    created filament-resoruce for contact

---
+++ app/Filament/Resources/Contacts/Schemas/ContactInfolist.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\Contacts\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class ContactInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('email')
+                    ->label('Email address')
+                    ->placeholder('-'),
+                TextEntry::make('phone'),
+                TextEntry::make('message')
+                    ->columnSpanFull(),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
